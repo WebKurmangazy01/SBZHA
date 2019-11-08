0c33e42c7ef3cbf898fb506eb9578bf4d017db6e
180107180 <49221685+180107180@users.noreply.github.com>
2019-11-08 23:58:04 +0600

    Update button click events
    1,2 ciphers

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ var putInPlus = {
   '+': (firstOper, secondOper) => firstOper + secondOper,
 
 } ;
+function putInOne(operator){
+	operator = 1;
+	document.form.textfield.value = document.form.textfield.value + operator;
+}
+function putInTwo(operator){
+	operator = 2;
+	document.form.textfield.value = document.form.textfield.value + operator;
+}
