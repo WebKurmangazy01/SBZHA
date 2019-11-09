7d41df6f1a14b028586d9e335696ddf98362bc22
Sultan-Bibarys <57250403+Sultan-Bibarys@users.noreply.github.com>
2019-11-09 14:05:10 +0600

    Add files via upload

---
+++ calculator/calcuscript.js
@@ -0,0 +1,16 @@
+/*var numver0 = 0;
+var number7 = 7;
+var number8 = 8;
+var number3 = 3;*/
+function putInSeven(number) {
+    document.form.textfield.value = document.form.textfield.value + number;
+}
+function putInEight(number){
+    document.form.textview.value = document.form.textview.value + number;
+}
+function putInThree(number){
+    document.form.textview.value = document.form.textview.value + number;
+}
+function putInZero(number){
+    document.form.textview.value = document.form.textview.value + number;
+}
