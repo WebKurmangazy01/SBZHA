ce2790dbe5ab6a499d8d9af9571b16456c4042e7
180107027ayanmiram <55638336+180107027ayanmiram@users.noreply.github.com>
2019-11-09 15:14:17 +0600

    Add files via upload
    updated!

---
+++ 180107027(1).js
@@ -0,0 +1,11 @@
+function putInFour(){
+	document.calculator.textfield.value += '4';
+}
+//This function putting 5 in a textfield on button onclick event
+function putInFive(){
+	document.calculator.textfield.value += '5';
+}
+//This function putting 6 in a textfield on button onclick event
+function putInSix(){
+	document.calculator.textfield.value += '6';
+}
