346d488deaede27b81a321b1b722905fac13df00
Sultan-Bibarys <57250403+Sultan-Bibarys@users.noreply.github.com>
2019-11-09 14:16:27 +0600

    update

---
+++ calcuscript.js
@@ -0,0 +1,25 @@
+/*var numver0 = 0;
+var number7 = 7;
+var number8 = 8;
+var number3 = 3;*/
+function putInOne(){
+	document.calculator.textfield.value += '1';
+}
+//This function putting 2 in a textfield on button onclick event
+function putInTwo(){
+	document.calculator.textfield.value += '2';
+}
+//This function putting 3 in a textfield on button onclick event
+function putInThree(){
+	document.calculator.textfield.value += '3';
+}
+function putInSeven(){
+	document.calculator.textfield.value += '7';
+}
+//This function putting 8 in a textfield on button onclick event
+function putInEight(){
+    document.calculator.textfield.value += '8';}
+    //This function putting 0 in a textfield on button onclick event
+function putInZero(){
+    document.calculator.textfield.value += '0';}
+    
